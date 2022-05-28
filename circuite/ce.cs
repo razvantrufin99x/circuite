fd3cdd5fb83e4d8717087a4e84ba32bf8ecc4e56
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-05-28 13:04:55 +0300

    ref returns

--- circuite/ce.cs
+++ circuite/ce.cs
@@ -13,69 +13,69 @@ namespace circuite
         public List<int> i = new List<int>();
         public List<int> o = new List<int>();
 
-        public int AND(int a, int b, int c)
+        public int AND(int a, int b, ref int c)
         {
             if (a == 1 && b == 1) { c = 1; return 1; }
             else { c = 0; return 0; }
         }
 
-        public int OR(int a, int b, int c)
+        public int OR(int a, int b, ref int c)
         {
             if (a == 1 || b == 1) { c = 1; return 1; }
             else { c = 0; return 0; }
         }
 
-        public int NOT(int a,  int c)
+        public int NOT(int a, ref int c)
         {
             if (a == 1 ) { c = 1; return 1; }
             else { c = 0; return 0; }
         }
 
-        public int BUFFER(int a, int c)
+        public int BUFFER(int a, ref int c)
         {
             if (a == 1) { c = 0; return 0; }
             else { c = 1; return 1; }
         }
 
-        public int NOR(int a, int b, int c)
+        public int NOR(int a, int b, ref int c)
         {
             if (a == 1 || b == 1) { c = 0; return 0; }
             else { c = 1; return 1; }
         }
 
-        public int XOR(int a, int b, int c)
+        public int XOR(int a, int b, ref int c)
         {
             if (a!=b && (a == 1 || b == 1)) { c = 1; return 1; }
             else { c = 0; return 0; }
         }
 
-        public int XNOR(int a, int b, int c)
+        public int XNOR(int a, int b, ref int c)
         {
             if (a != b && (a == 1 || b == 1)) { c = 0; return 0; }
             else { c = 1; return 1; }
         }
 
-        public int NAND(int a, int b, int c)
+        public int NAND(int a, int b, ref int c)
         {
             if  (a == 1 && b == 1) { c = 0; return 0; }
             else { c = 1; return 1; }
         }
 
-        public int ANDmx(ref List<int> im, int c)
+        public int ANDmx(ref List<int> im, ref int c)
         {
             if (im[0] == 1 && im[1] == 1 && im[2] == 1) { c = 1; return 1; }
             else { c = 1; return 0; }
         }
 
-        public int MUX(int a, int b, int s, int z)
+        public int MUX(int a, int b, int s, ref int z)
         {
             int sb = 0;
             int asn = 0;
             int sn = 0;
-            this.AND(a, b, sb);
-            this.NOT(s, sn);
-            this.AND(a, sn, asn);
-            this.OR(asn, sb, z);
+            this.AND(a, b, ref sb);
+            this.NOT(s, ref sn);
+            this.AND(a, sn, ref asn);
+            this.OR(asn, sb, ref z);
             return 1;
         }
 
